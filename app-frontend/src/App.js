7fb212155498651f9d4770e26c6820f17de9eb3d
Ilham Akbar <ilhamije@gmail.com>
2022-10-28 23:05:51 +0700

    frontend - added data from backend

--- app-frontend/src/App.js
+++ app-frontend/src/App.js
@@ -1,19 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import useAxios from 'axios-hooks';
+
 import MapWithPlaceholder from './Map';
-import pointData from './pointData.json';
+import './App.css';
 
 function App() {
+  const [{ data, loading, error }] = useAxios(
+    'http://127.0.0.1:5001/point/'
+  )
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error! Unable to retrieve data from backend</p>
+
+  console.log(data);
+  var pointData = data;
+  console.log(pointData);
 
   return (
     <>
       <h1>Drone Byakugan</h1>
       <MapWithPlaceholder pointData={pointData}  />
     </>
-    // <div className="App">
-    //   <header className="App-header">
-    //   </header>
-    // </div>
   );
 }
 
