be5afca090da60f70df12be423a90d037d144147
Ilham Akbar <ilhamije@gmail.com>
2022-11-01 22:03:06 +0700

    Added - Gdrive download_folder

--- worker/img_downloader.py
+++ worker/img_downloader.py
@@ -1,7 +1,15 @@
 import gdown
-from datetime import datetime
 
-url = <string-of-drive-url-folder>
+def main():
+    url = 'https://drive.google.com/drive/folders/1C9UFaMVp9wkHML32hh7CN2McUUwCoxp5'
 
-filename = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-gdown.download_folder(url, quiet=True, use_cookies=False) 
+    output_path = 'gdrivedata'
+    if gdown.download_folder(url, output=output_path, quiet=False, use_cookies=False):
+        return "DONE"
+    else:
+        return "Not working."
+
+if __name__ == "__main__":
+    print("="*20+"Start Downloading"+"="*10)
+    main()
+    print("="*20+"End"+"="*10)
